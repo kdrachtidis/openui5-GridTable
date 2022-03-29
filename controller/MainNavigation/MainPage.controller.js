bbe90598506c8171cc8c6e2102240dc93464ed5a
Konstantinos Drachtidis <konstantinos.drachtidis@sap.com>
2022-03-29 14:45:59 +0200

    over JSON content

--- controller/MainNavigation/MainPage.controller.js
+++ controller/MainNavigation/MainPage.controller.js
@@ -3,7 +3,23 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
 
-    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage");
+    var Controller = Controller.extend("DemoUXapp.controller.MainNavigation.MainPage", {
+        onInit: function () {
+            var sURL = "/";
+            var amodel = new sap.ui.model.odata.ODataModel(sURL, true);
+            var oModel = new sap.ui.model.json.JSONModel();
+            amodel.read("/Objects-StoryBuilder", {
+                success: function (oRetrievedResult) {
+                    oModel.setData(oRetrievedResult);
+                },
+                error: function (oError) { alert(`Error ${oError}`); }
+            });
+            this.getView().setModel(oModel);
+        },
+        getRouter: function () {
+            return sap.ui.core.UIComponent.getRouterFor(this);
+        }
+    });
 
     return Controller;
 });
